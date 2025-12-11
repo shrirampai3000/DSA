f4c2e0abd4e096e55170c642115caba309c2f3e4
shrirampai3000 <shrirampai3000@gmail.com>
2025-12-11 19:27:59 +0530

    Time: 5 ms (3.90%) | Memory: 50.6 MB (5.05%) - LeetSync

---
+++ 344-reverse-string/reverse-string.java
@@ -0,0 +1,16 @@
+import java.util.Stack;
+
+class Solution {
+    public void reverseString(char[] s) {
+        Stack<Character> stack = new Stack<>();
+
+        for (char c : s) {
+            stack.push(c);
+        }
+
+        int i = 0;
+        while (!stack.isEmpty()) {
+            s[i++] = stack.pop();
+        }
+    }
+}
