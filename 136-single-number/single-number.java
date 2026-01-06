091bce7a9cc79b7622a098d624c8c924527b2897
Shriram Krishnakumar Pai <shrirampai3000@gmail.com>
2026-01-06 18:46:37 +0530

    Time: 1 ms (99.87%) | Memory: 47.2 MB (16.73%) - LeetSync

--- 136-single-number/single-number.java
+++ 136-single-number/single-number.java
@@ -1,13 +1,10 @@
 class Solution {
     public int singleNumber(int[] nums) {
-        HashSet<Integer> set=new HashSet<>();
+        int ans=0;
+
         for(int num:nums){
-            if(set.contains(num)){
-                set.remove(num);
-            }
-            else{
-                set.add(num);
-            }
-        }return set.iterator().next();
+            ans=ans^num;
+        }
+        return ans;
     }
 }
