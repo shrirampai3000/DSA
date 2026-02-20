91c42d1023c6e985112413bb0e19a790a9a41630
Shriram Krishnakumar Pai <shrirampai3000@gmail.com>
2026-02-20 21:36:43 +0530

    Time: 11 ms (37.12%) | Memory: 59.5 MB (57.94%) - LeetSync

--- 148-sort-list/sort-list.java
+++ 148-sort-list/sort-list.java
@@ -8,48 +8,66 @@
  *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
  * }
  */
+
 class Solution {
-    public ListNode getMid(ListNode head) {
+
+    private ListNode getMid(ListNode head){
         ListNode slow = head;
-        ListNode fast = head.next; 
-        while (fast != null && fast.next != null) {
+        ListNode fast = head.next;
+
+        while(fast != null && fast.next != null){
             slow = slow.next;
             fast = fast.next.next;
         }
-        return slow; 
+        return slow;
     }
 
-    private ListNode merge(ListNode head1, ListNode head2) {
-        ListNode dummy = new ListNode(-1);
-        ListNode temp = dummy;
+    private ListNode merge(ListNode head1, ListNode head2){
+        ListNode mergedLL = new ListNode(-1);
+        ListNode temp = mergedLL;
 
-        while (head1 != null && head2 != null) {
-            if (head1.val <= head2.val) {
+        while(head1 != null && head2 != null){
+            if(head1.val <= head2.val){
                 temp.next = head1;
                 head1 = head1.next;
-            } else {
+            }else{
                 temp.next = head2;
                 head2 = head2.next;
             }
             temp = temp.next;
         }
 
-        if (head1 != null) temp.next = head1;
-        if (head2 != null) temp.next = head2;
+        while(head1 != null){
+            temp.next = head1;
+            head1 = head1.next;
+            temp = temp.next;
+        }
 
-        return dummy.next;
+        while(head2 != null){
+            temp.next = head2;
+            head2 = head2.next;
+            temp = temp.next;
+        }
+
+        return mergedLL.next;
     }
 
-    public ListNode sortList(ListNode head) {
-        if (head == null || head.next == null) return head;
+    private ListNode mergeSort(ListNode head){
+        if(head == null || head.next == null){
+            return head;
+        }
 
         ListNode mid = getMid(head);
         ListNode rightHead = mid.next;
         mid.next = null;
 
-        ListNode left = sortList(head);
-        ListNode right = sortList(rightHead);
+        ListNode newLeft = mergeSort(head);
+        ListNode newRight = mergeSort(rightHead);
 
-        return merge(left, right);
+        return merge(newLeft, newRight);
+    }
+
+    public ListNode sortList(ListNode head) {
+        return mergeSort(head);
     }
 }
