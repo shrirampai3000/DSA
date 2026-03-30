b11209ca0f0b2301f45267fcab84c88f8e7946c3
Shriram Krishnakumar Pai <shrirampai3000@gmail.com>
2026-03-31 00:56:29 +0530

    Time: 767 ms (10.77%) | Memory: 59.6 MB (23.31%) - LeetSync

--- 15-3sum/3sum.java
+++ 15-3sum/3sum.java
@@ -1,33 +1,30 @@
 class Solution {
     public List<List<Integer>> threeSum(int[] nums) {
-        List<List<Integer>> result = new ArrayList<>();
         Arrays.sort(nums);
-
-        for (int i = 0; i < nums.length - 2; i++) {
-            if (i > 0 && nums[i] == nums[i - 1]) continue;
-
-            int left = i + 1;
-            int right = nums.length - 1;
-
-            while (left < right) {
-                int sum = nums[i] + nums[left] + nums[right];
-
-                if (sum == 0) {
-                    result.add(Arrays.asList(nums[i], nums[left], nums[right]));
-
-                    while (left < right && nums[left] == nums[left + 1]) left++;
-                    while (left < right && nums[right] == nums[right - 1]) right--;
-
-                    left++;
-                    right--;
-                } else if (sum < 0) {
-                    left++;
-                } else {
-                    right--;
-                }
-            }
+        Map<Integer, Integer> count = new HashMap<>();
+        for (int num : nums) {
+            count.put(num, count.getOrDefault(num, 0) + 1);
         }
 
-        return result;
+        List<List<Integer>> res = new ArrayList<>();
+        for (int i = 0; i < nums.length; i++) {
+            count.put(nums[i], count.get(nums[i]) - 1);
+            if (i > 0 && nums[i] == nums[i - 1]) continue;
+
+            for (int j = i + 1; j < nums.length; j++) {
+                count.put(nums[j], count.get(nums[j]) - 1);
+                if (j > i + 1 && nums[j] == nums[j - 1]) continue;
+
+                int target = -(nums[i] + nums[j]);
+                if (count.getOrDefault(target, 0) > 0) {
+                    res.add(Arrays.asList(nums[i], nums[j], target));
+                }
+            }
+
+            for (int j = i + 1; j < nums.length; j++) {
+                count.put(nums[j], count.get(nums[j]) + 1);
+            }
+        }
+        return res;
     }
 }
