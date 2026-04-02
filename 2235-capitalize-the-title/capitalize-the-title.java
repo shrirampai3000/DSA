97dfb67986f6df974224c07e4312c137e449bf50
Shriram Krishnakumar Pai <shrirampai3000@gmail.com>
2026-04-02 22:30:13 +0530

    Time: 3 ms (80.83%) | Memory: 43.7 MB (75.27%) - LeetSync

---
+++ 2235-capitalize-the-title/capitalize-the-title.java
@@ -0,0 +1,23 @@
+class Solution {
+    public String capitalizeTitle(String title) {
+        String[] words = title.split(" ");
+        StringBuilder sb = new StringBuilder();
+
+        for (int i = 0; i < words.length; i++) {
+            String word = words[i];
+
+            if (word.length() <= 2) {
+                sb.append(word.toLowerCase());
+            } else {
+                sb.append(Character.toUpperCase(word.charAt(0)));
+                sb.append(word.substring(1).toLowerCase());
+            }
+
+            if (i != words.length - 1) {
+                sb.append(" ");
+            }
+        }
+
+        return sb.toString();
+    }
+}
