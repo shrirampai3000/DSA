05065ffba8f9899a152edf67459f9ea2bd6c52d1
Shriram Krishnakumar Pai <shrirampai3000@gmail.com>
2026-06-08 07:08:16 +0530

    Time: 5 ms (44.96%) | Memory: 94.4 MB (65.67%) - LeetSync

--- 234-palindrome-linked-list/palindrome-linked-list.java
+++ 234-palindrome-linked-list/palindrome-linked-list.java
@@ -8,26 +8,47 @@
  *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
  * }
  */
-public class Solution {
+class Solution {
+
+    public ListNode findMid(ListNode head) {
+        ListNode slow = head;
+        ListNode fast = head;
+
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+
+        return slow;
+    }
 
     public boolean isPalindrome(ListNode head) {
-        List<Integer> arr = new ArrayList<>();
-        ListNode cur = head;
-        while (cur != null) {
-            arr.add(cur.val);
-            cur = cur.next;
+        if (head == null || head.next == null) {
+            return true;
+        }
+        ListNode mid = findMid(head);
+
+        ListNode prev = null;
+        ListNode curr = mid;
+
+        while (curr != null) {
+            ListNode next = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = next;
         }
 
-        int l = 0, r = arr.size() - 1;
-        while (l < r) {
-            if (!arr.get(l).equals(arr.get(r))) {
+        ListNode left = head;
+        ListNode right = prev;
+
+        while (right != null) {
+            if (left.val != right.val) {
                 return false;
             }
-            l++;
-            r--;
-        }
 
+            left = left.next;
+            right = right.next;
+        }
         return true;
     }
-    
 }
