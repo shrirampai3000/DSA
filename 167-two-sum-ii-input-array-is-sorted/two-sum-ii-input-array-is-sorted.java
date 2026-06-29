62168b69e4d170f8536241172f1e53a95abd7456
Shriram Krishnakumar Pai <shrirampai3000@gmail.com>
2026-06-29 22:35:38 +0530

    Time: 2 ms (96.00%) | Memory: 48.6 MB (24.27%) - LeetSync

--- 167-two-sum-ii-input-array-is-sorted/two-sum-ii-input-array-is-sorted.java
+++ 167-two-sum-ii-input-array-is-sorted/two-sum-ii-input-array-is-sorted.java
@@ -5,7 +5,6 @@ class Solution {
 
         while(left<right){
             int sum=numbers[left]+numbers[right];
-
             if(sum==target){
                 return new int[]{left+1,right+1};
             }else if(sum<target){
@@ -13,7 +12,6 @@ class Solution {
             }else{
                 right--;
             }
-        }
-        return new int[]{};
+        }return new int[]{-1,1};
     }
 }
